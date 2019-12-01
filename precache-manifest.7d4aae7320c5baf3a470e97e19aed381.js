e91e0b4d49b9199a6ee6c8764109e5c69e02c4b3
Leonardo Palmeiro <leonardo@palmeiro.com>
2019-12-01 08:08:06 +0100

    Updates

---
+++ precache-manifest.7d4aae7320c5baf3a470e97e19aed381.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd5ccae6206821d92e1a2df572466fd",
+    "url": "/KeytreeApp/index.html"
+  },
+  {
+    "revision": "95b075e64ba4da554af0",
+    "url": "/KeytreeApp/static/css/2.327aea02.chunk.css"
+  },
+  {
+    "revision": "677e9a27af8243c32a16",
+    "url": "/KeytreeApp/static/css/main.a3f15a62.chunk.css"
+  },
+  {
+    "revision": "95b075e64ba4da554af0",
+    "url": "/KeytreeApp/static/js/2.4aa6992d.chunk.js"
+  },
+  {
+    "revision": "677e9a27af8243c32a16",
+    "url": "/KeytreeApp/static/js/main.7d968c66.chunk.js"
+  },
+  {
+    "revision": "72cc747a8fd7c02466b6",
+    "url": "/KeytreeApp/static/js/runtime-main.26218d39.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/KeytreeApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/KeytreeApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/KeytreeApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/KeytreeApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/KeytreeApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e9cf698fc53ab4d3698481f10c75669a",
+    "url": "/KeytreeApp/static/media/notfound.e9cf698f.svg"
+  }
+]);
